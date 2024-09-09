75e509b5d06bb0424cea1a1785d7176ee22fb411
cemzzz <koreaok01@gmail.com>
2024-09-09 21:39:42 +0900

    news api 가져오기 작업 및 웹화면 초기 설계

---
+++ main.js
@@ -0,0 +1,41 @@
+const API_KEY=`b0359ebba28749d1a115ccc565bef4d1`
+
+let newsList=[]
+
+const getNews = async () => {
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const response =  await fetch(url)
+    const data = await response.json()
+    newsList = data.articles.filter(news => news.url && news.urlToImage);
+    render()
+}
+
+const render = () => {
+    const newsHTML = newsList.map(news => {
+    
+        const title = news.title.includes(' - ') 
+            ? news.title.split(' - ')[0].trim() 
+            : news.title;
+
+        const description = news.description
+            ? (news.description.lenght > 200
+                ? news.description.substring(0, 200) + '...'
+                : news.description)
+            : "No description"
+
+        return`
+        <div class="row news-list">
+            <div class="col-lg-4">
+                <img class="news-img" src=${news.urlToImage} />
+            </div>
+            <div class="col-lg-8">
+                <h2>${title}</h2>
+                <p>${description}</p>
+                <div>${news.source.name} * ${new Date(news.publishedAt).toLocaleDateString()}</div>
+            </div>
+        </div>`}).join('');
+
+    document.getElementById("news-board").innerHTML = newsHTML
+}
+
+getNews()
