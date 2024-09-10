9a8268f40867189cbb0deecd6e98ba092e8b29c2
cemzzz <koreaok01@gmail.com>
2024-09-10 19:07:26 +0900

    검색 기능 구현

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const API_KEY=`b0359ebba28749d1a115ccc565bef4d1`
 
 let newsList=[]
 const menus = document.querySelectorAll(".menus button")
-menus.forEach(menu => menu.addEventListener("click", (event)=>newsCategory(event)))
+const searchInput = document.getElementById("search-input")
+
+menus.forEach(menu => menu.addEventListener("click", (event)=>newsCategory(event))) // 메뉴 클릭 이벤트
+
+searchInput.addEventListener("keydown", (event) => {
+    if(event.key === "Enter"){
+        newsKeyword()
+    }
+})
 
 const getNews = async () => {
     const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
@@ -21,7 +29,25 @@ const newsCategory = async (event) => {
     render()
 }
 
+const newsKeyword = async () => {
+    const keyword = document.getElementById("search-input").value
+    const url =  new URL(`https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&apiKey=${API_KEY}`)
+    const response =  await fetch(url)
+    const data = await response.json()
+    newsList = data.articles.filter(news => news.url && news.urlToImage);
+    render()
+}
+
 const render = () => {
+    // 검색 결과가 없는 경우
+    if (newsList.length === 0) {
+        document.getElementById("news-board").innerHTML = `
+            <div class="no-News-List">
+                <div>관련 기사가 없습니다.</div>
+            </div>
+        `;
+        return;
+    }
     const newsHTML = newsList.map(news => {
     
         const title = news.title.includes(' - ') 
