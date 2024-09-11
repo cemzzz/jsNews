884c1e6ab6c437d2db2246ea812b51282692f0e0
cemzzz <koreaok01@gmail.com>
2024-09-11 14:46:42 +0900

    중복 코드 정리

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const API_KEY=`b0359ebba28749d1a115ccc565bef4d1`
+//const API_KEY=`pub_53149b62cd2c643ab834f9bceb73ef4077a59`
 
 let newsList=[]
+let url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`) // 공통적으로 쓰일 url 기본 값은 head
 const menus = document.querySelectorAll(".menus button")
 const searchInput = document.getElementById("search-input")
 
@@ -12,53 +14,51 @@ searchInput.addEventListener("keydown", (event) => {
     }
 })
 
-const getNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+const getNewsData = async() =>{
     const response =  await fetch(url)
     const data = await response.json()
     newsList = data.articles.filter(news => news.url && news.urlToImage);
     render()
+}
+
+const getNews = async () => {
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    getNewsData()
 }
 
 const newsCategory = async (event) => {
     const category = event.target.textContent
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`)
-    const response =  await fetch(url)
-    const data = await response.json()
-    newsList = data.articles.filter(news => news.url && news.urlToImage);
-    render()
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`)
+    getNewsData()
 }
 
 const newsKeyword = async () => {
     const keyword = document.getElementById("search-input").value
-    const url =  new URL(`https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&apiKey=${API_KEY}`)
-    const response =  await fetch(url)
-    const data = await response.json()
-    newsList = data.articles.filter(news => news.url && news.urlToImage);
-    render()
+    url =  new URL(`https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&apiKey=${API_KEY}`)
+    getNewsData()
 }
 
 const render = () => {
     // 검색 결과가 없는 경우
     if (newsList.length === 0) {
         document.getElementById("news-board").innerHTML = `
-            <div class="no-News-List">
+            <div class="no-news-list">
                 <div>관련 기사가 없습니다.</div>
             </div>
         `;
         return;
     }
     const newsHTML = newsList.map(news => {
-    
+
         const title = news.title.includes(' - ') 
             ? news.title.split(' - ')[0].trim() 
             : news.title;
 
         const description = news.description
-            ? (news.description.lenght > 200
-                ? news.description.substring(0, 200) + '...'
-                : news.description)
-            : "No description"
+        ? (news.description.length > 200
+            ? news.description.substring(0, 200) + '...'
+            : news.description)
+        : "no description.";
 
         return`
         <div class="row news-list">
